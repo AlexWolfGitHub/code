05e56a36cb85812c37a403e770e800960b202d80
AlexWolfGithub <601355245@qq.com>
2018-04-12 10:44:08 +0800

    前端

---
+++ src/main/resources/static/app/system/templete1/TempleteIndex1Moudle.js
@@ -0,0 +1,9 @@
+define([
+    'angular',
+    'jquery',
+    'bootstrap'
+], function (angular) {
+    var app = angular.module('Templete1IndexModule', []);
+    app.controller('Templete1IndexModule', function ($scope) {
+    });
+});
