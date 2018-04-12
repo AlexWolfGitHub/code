e74eea6a6910cfe841505d0b2e6bd6e7c474098b
AlexWolfGithub <601355245@qq.com>
2018-04-12 10:45:33 +0800

    改进

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -5,7 +5,16 @@ require.config({
     paths: {
         "jquery": window.ctx + "/bower_components/jquery/dist/jquery.min",
         "bootstrap": window.ctx + "/bower_components/bootstrap/dist/js/bootstrap.min",
-        "angular": window.ctx + "/bower_components/angular/angular"
+        "angular": window.ctx + "/bower_components/angular/angular",
+        "jqueryEasing": window.ctx + "/static/templete1/js/jquery.easing.min",
+        "jqueryStellar": window.ctx + "/static/templete1/js/jquery.stellar",
+        "jqueryAppear": window.ctx + "/static/templete1/js/jquery.appear",
+        "jqueryNicescroll": window.ctx + "/static/templete1/js/jquery.nicescroll.min",
+        "jqueryCountTo": window.ctx + "/static/templete1/js/jquery.countTo",
+        "modernizr": window.ctx + "/static/templete1/js/jquery.shuffle.modernizr",
+        "jqueryShuffle": window.ctx + "/static/templete1/js/jquery.shuffle",
+        "jowlCarousel": window.ctx + "/static/templete1/js/owl.carousel",
+        "jqueryAjaxchimp": window.ctx + "/static/templete1/js/jquery.ajaxchimp.min"
     },
     shim: {
         "angular": {
--- src/main/resources/static/app/system/templete1/TempleteIndex1Moudle.js
+++ src/main/resources/static/app/system/templete1/TempleteIndex1Moudle.js
@@ -1,7 +1,15 @@
 define([
-    'angular',
-    'jquery',
-    'bootstrap'
+    'bootstrap',
+    "jqueryEasing",
+    "jqueryStellar",
+    "jqueryAppear",
+    "jqueryNicescroll",
+    "jqueryCountTo",
+    "modernizr",
+    "jqueryShuffle",
+    "jowlCarousel",
+    "jqueryAjaxchimp",
+    "/static/templete1/js/script.js"
 ], function (angular) {
     var app = angular.module('Templete1IndexModule', []);
     app.controller('Templete1IndexModule', function ($scope) {
